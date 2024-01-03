4542e05dbba3028cc31f234414f4fb61dfc00163
DjWeBack <85476279+EofExceptionJAVA@users.noreply.github.com>
2024-01-03 09:37:53 +0900

    Add files via upload

---
+++ Common/DataAccess/OutputValues.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalToExApi.Common.DataAccess
+{
+	public class OutputValues
+	{
+		private int _returnValue;
+		private Dictionary<string, IDbDataParameter> _outputParams;
+
+		/// <summary>
+		/// 반환 값
+		/// </summary>
+		public int ReturnValue
+		{
+			get { return _returnValue; }
+			set { _returnValue = value; }
+		}
+		/// <summary>
+		/// outputs
+		/// </summary>
+		public Dictionary<string, IDbDataParameter> OutputParams
+		{
+			get { return _outputParams; }
+			set { _outputParams = value; }
+		}
+
+		/// <summary>
+		/// constructor
+		/// </summary>
+		public OutputValues()
+		{
+			_outputParams = new Dictionary<string, IDbDataParameter>();
+		}
+	}
+}
